e7fcbf132cf938bfff2de876487e9e078e25657d
Hsekharsahoo <79087876+Hsekharsahoo@users.noreply.github.com>
2021-05-15 10:36:12 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,47 @@
+/*
+------------------------
+Author:  hsekharsahoo
+Started: [10/04/2021]
+------------------------
+*/
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll   long long int
+#define ff   first
+#define ss   second
+#define vi   vector<int>
+#define pi   pair<int,int>
+#define mp   make_pair
+#define pb   push_back
+#define ppb  pop_back
+#define rep(i,a,b) for(int i=a;i<b;++i)
+#define all(x)  x.begin(),x.end()
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const   int M=1e9+7;
+ 
+void solve()
+{  
+  
+} 
+ 
+int main()
+{
+ fastio
+ int t=1;
+ //cin>>t;
+ while(t--)
+ {
+   solve();
+ }
+}
+/* Things to look for
+  * int overflow, array bounds,inputs
+  * dont spend more than 30mins on A/B even while practicing
+  * special cases (n==1?)
+  * do write return when required
+  * do smth instead of nothing and be organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH
+*/
