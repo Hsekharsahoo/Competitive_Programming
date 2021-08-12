c3128ccebf94fdb32d2dd6162331c3e770680fd8
Himanshu Sekhar Sahoo <79087876+Hsekharsahoo@users.noreply.github.com>
2021-08-12 12:27:11 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,56 +1,57 @@
-/*
-------------------------
-Author : hsekharsahoo
-Started: [10/04/2021]
-
-" THINK MORE, CODE LESS "
-------------------------
-*/
-#include<bits/stdc++.h>
+#include"bits/stdc++.h"
 using namespace std;
 
-#define ll   long long int
-#define ff   first
-#define ss   second
-#define vi   vector<int>
-#define pii  pair<int,int>
-#define mp   make_pair
-#define pb   push_back
-#define ppb  pop_back
+#define int         long long
+#define ff          first
+#define ss          second
+#define vi          vector<int>
+#define pii         pair<int,int>
+#define mp          make_pair
+#define pb          push_back
+#define ppb         pop_back
+#define sz(x)       (int)x.size()
+#define ppc(x)      __builtin_popcount(x)
+#define ppcll(x)    __builtin_popcountll(x)
+#define all(x)      x.begin(),x.end()
+#define prec(x)     fixed<<setprecision(x)
+#define endl        "\n"
+const   int M       =1e9+7;
+const   double PI   =3.14159265358979323846;
 
 #define rep(i,a,b)  for(int i=a;i<b;++i)
 #define rrep(i,a,b) for(int i=a;i>=b;i--)
 #define each(x,v)   for(auto &x: v)
 
-#define all(x)   x.begin(),x.end()
-#define prec(x)  fixed<<setprecision(x)
-#define endl     "\n"
-#define fastio   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-const   int M    =1e9+7;
-
 #ifndef ONLINE_JUDGE
-#define debug(x) cerr<<#x<<":"<<x<<endl;
+#define deb(x) cerr<<#x<<": "<<x<<endl;
 #else
-#define debug(x)
+#define deb(x)
 #endif
 
 void solve()
-{  
+{ 
   
-} 
+}
 
-int main()
+signed main()
 {
+ ios_base::sync_with_stdio(0); 
+ cin.tie(0); cout.tie(0);
 
- fastio
  #ifndef ONLINE_JUDGE
     freopen("error.txt","w",stderr);
  #endif
 
  int t=1;
- //cin>>t;
+ // cin>>t;
  while(t--)
  {
   solve();
  }
+ 
+ // for(int i=1;i<=t;i++)
+ // {
+ //   cout<<"Case #"<<i<<": ";
+ //   solve();
+ // } 
 }
