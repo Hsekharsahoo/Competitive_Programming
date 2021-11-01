ce709af1a25f8018c217229001325b45cbf93023
Himanshu Sekhar Sahoo <79087876+Hsekharsahoo@users.noreply.github.com>
2021-11-01 20:09:18 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+#define int          long long
+#define ff           first
+#define ss           second
+#define sz(x)        (int)(x.size())
+#define all(x)       x.begin(),x.end()
+#define nl           '\n'
+#define rep(i,a,b)   for(int i=a;i<b;++i)
+#define rrep(i,a,b)  for(int i=a-1;i>=b;i--)
+
+using namespace std;
+const int M = 1e9 + 7;
+
+#ifdef POTATO
+#define deb(x) cerr<<#x<<": "<<x<<nl;
+#else
+#define deb(x)
+#endif
+
+void potato(){
+   ${1:}
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef POTATO
+    freopen("error.txt", "w", stderr);
+#endif
+    int _test = 1;
+    ${2:}//cin>>_test;
+    while(_test--){
+       potato();
+    }
+}
